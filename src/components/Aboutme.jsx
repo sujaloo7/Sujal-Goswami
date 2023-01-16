44f2d15853f07e48764b406a7c55894f57bbbf96
sujaloo7 <sujal.ktech@gmail.com>
2023-01-16 18:14:54 +0530

    ui refresh

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import AOS from "aos";
 import "aos/dist/aos.css";
+import CountUp from 'react-countup';
 
 const Aboutme = () => {
     useEffect(() => {
@@ -15,26 +16,31 @@ const Aboutme = () => {
                 <div className="container-fluid aboutme">
                     <div className="container">
                         <h2 className='mt-5 mb-5 '>About Me</h2>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-6">
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <div className="card">
-                                        <h5>50</h5>
-                                        <p>Happy Clients</p>
+                        <div className="row">
+                            <div className="col-sm-5">
+                                <div className="row">
+                                    <div className="col-sm-6">
+                                        <div className="card ">
+                                            <div>
+                                                <h5><CountUp end={50} /></h5>
+                                                <p>Happy Clients</p>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="col-sm-6">
-                                    <div className="card">
-                                        <h5>15</h5>
-                                        <p>Projects Done</p>
+                                    <div className="col-sm-6">
+                                        <div className="card">
+                                            <div>
+                                                <h5><CountUp end={15} /></h5>
+                                                <p>Projects Done</p>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
 
+                                </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         </>
